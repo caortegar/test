dbde3c8f4bdaddce039dbaa5b90485c2e49a6fab
caortegar <36177055+caortegar@users.noreply.github.com>
2018-02-24 08:01:51 -0500

    Create ejemplo1.py

---
+++ ejemplo1.py
@@ -0,0 +1,17 @@
+print('This is an example')
+for n in range (9): 
+  print(n)
+  
+  
+  
+class listfilter:
+    def f(x):
+        i = 0
+        while i <= len(x)-1:
+             if bool(x[i]) == True:
+                print(x[i])
+             i = i + 1
+
+x = ['v', [], '3', 'a', []]
+
+listfilter.f(x)
